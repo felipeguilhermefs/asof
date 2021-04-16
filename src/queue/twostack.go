c7abd28283167e7a9834642f5b7afdaa0e4054fc
Felipe Flores <felipeguilhermefs@gmail.com>
2021-04-16 17:26:43 -0300

    Implement a queue with 2 stacks

---
+++ src/queue/twostack.go
@@ -0,0 +1,45 @@
+package queue
+
+import "github.com/felipeguilhermefs/asof/stack"
+
+// TwoStackQueue queue (FIFO) implemented with 2 stacks
+// The idea is that:
+//  - we enqueue to stack 1
+//  - we dequeue from stack 2
+//  - when there is nothing in stack 2
+//    we pop from stack 1 and push to stack 2
+type TwoStackQueue struct {
+	stack1 stack.Stack
+	stack2 stack.Stack
+}
+
+// Len total of items in queue
+func (q *TwoStackQueue) Len() int {
+	return q.stack1.Len() + q.stack2.Len()
+}
+
+// Enqueue insert item at the end of the queue
+func (q *TwoStackQueue) Enqueue(item int) {
+	// always push to the stack 1
+	q.stack1.Push(item)
+}
+
+// Dequeue get item at the start of the queue
+func (q *TwoStackQueue) Dequeue() (int, bool) {
+	// we pop from stack 2 if there is something there
+	if item, ok := q.stack2.Pop(); ok {
+		return item, true
+	}
+
+	// otherwise we get everything from stack 1 and push to stack 2
+	// given that they pop LIFO their order will be reversed and
+	// we'll get the FIFO behaviour
+	item, ok := q.stack1.Pop()
+	for ok {
+		q.stack2.Push(item)
+		item, ok = q.stack1.Pop()
+	}
+
+	// now we can just pop from stack 2
+	return q.stack2.Pop()
+}
---
+++ src/queue/twostack_test.go
@@ -0,0 +1,43 @@
+package queue
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+// TestTwoStackQueue
+func TestTwoStackQueue(t *testing.T) {
+	seed := time.Now().UnixNano()
+	rand.Seed(seed)
+
+	for name, list := range map[string][]int{
+		"random-len10":   rand.Perm(10),
+		"random-len1000": rand.Perm(1000),
+	} {
+		t.Run(name, func(t *testing.T) {
+			queue := TwoStackQueue{}
+
+			for _, item := range list {
+				queue.Enqueue(item)
+			}
+
+			if queue.Len() != len(list) {
+				t.Fatalf("Enqueue error: got %d; want %d", queue.Len(), len(list))
+			}
+
+			for i := range list {
+				item, ok := queue.Dequeue()
+				if !ok || item != list[i] {
+					t.Fatalf("Dequeue error: ok %v; index %d", ok, i)
+				}
+			}
+
+			_, ok := queue.Dequeue()
+			if ok {
+				t.Fatalf("Dequeue empty error: should be empty")
+			}
+
+		})
+	}
+}
