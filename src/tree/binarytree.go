78f8029784646919bb660c1491dd763ff1293de5
Felipe Flores <felipeguilhermefs@gmail.com>
2021-04-26 10:11:46 -0300

    Add traverse orders to binary tree

--- src/tree/binarytree.go
+++ src/tree/binarytree.go
@@ -21,7 +21,7 @@ func (bt *BinaryTree) Insert(value int) {
 }
 
 func (bt *BinaryTree) insert(n *node, newNode *node) {
-	if n.value > newNode.value {
+	if n.value < newNode.value {
 		if n.right == nil {
 			n.right = newNode
 		} else {
@@ -49,9 +49,63 @@ func (bt *BinaryTree) contains(n *node, value int) bool {
 		return true
 	}
 
-	if n.value > value {
+	if n.value < value {
 		return bt.contains(n.right, value)
 	} else {
 		return bt.contains(n.left, value)
 	}
+}
+
+const (
+	InOrder   TraverseOrder = 0
+	PreOrder  TraverseOrder = 1
+	PostOrder TraverseOrder = 2
+)
+
+type TraverseOrder = int
+
+type TraverseConsumer = func(int)
+
+func (bt *BinaryTree) Traverse(order TraverseOrder, consumer TraverseConsumer) {
+	if order == InOrder {
+		bt.inOrderTraverse(bt.root, consumer)
+		return
+	}
+
+	if order == PreOrder {
+		bt.preOrderTraverse(bt.root, consumer)
+		return
+	}
+
+	bt.postOrderTraverse(bt.root, consumer)
+}
+
+func (bt *BinaryTree) inOrderTraverse(n *node, consumer TraverseConsumer) {
+	if n == nil {
+		return
+	}
+
+	bt.inOrderTraverse(n.left, consumer)
+	consumer(n.value)
+	bt.inOrderTraverse(n.right, consumer)
+}
+
+func (bt *BinaryTree) preOrderTraverse(n *node, consumer TraverseConsumer) {
+	if n == nil {
+		return
+	}
+
+	consumer(n.value)
+	bt.preOrderTraverse(n.left, consumer)
+	bt.preOrderTraverse(n.right, consumer)
+}
+
+func (bt *BinaryTree) postOrderTraverse(n *node, consumer TraverseConsumer) {
+	if n == nil {
+		return
+	}
+
+	bt.postOrderTraverse(n.left, consumer)
+	bt.postOrderTraverse(n.right, consumer)
+	consumer(n.value)
 }
--- src/tree/binarytree_test.go
+++ src/tree/binarytree_test.go
@@ -23,4 +23,73 @@ func TestBinaryTree(t *testing.T) {
 			t.Fatal("Found data that it shouldn't")
 		}
 	})
+
+	t.Run("should traverse In Order", func(t *testing.T) {
+		testData := []int{2, 1, 4, 3, 5}
+
+		bt := BinaryTree{}
+
+		for _, data := range testData {
+			bt.Insert(data)
+		}
+
+		visited := []int{}
+
+		bt.Traverse(InOrder, func(value int) {
+			visited = append(visited, value)
+		})
+
+		expected := []int{1, 2, 3, 4, 5}
+		for i := range expected {
+			if visited[i] != expected[i] {
+				t.Fatalf("Unexpected: index= %d expected= %v visited= %v", i, expected, visited)
+			}
+		}
+	})
+
+	t.Run("should traverse Pre Order", func(t *testing.T) {
+		testData := []int{2, 1, 4, 3, 5}
+
+		bt := BinaryTree{}
+
+		for _, data := range testData {
+			bt.Insert(data)
+		}
+
+		visited := []int{}
+
+		bt.Traverse(PreOrder, func(value int) {
+			visited = append(visited, value)
+		})
+
+		expected := []int{2, 1, 4, 3, 5}
+		for i := range expected {
+			if visited[i] != expected[i] {
+				t.Fatalf("Unexpected: index= %d expected= %v visited= %v", i, expected, visited)
+			}
+		}
+	})
+
+	t.Run("should traverse Post Order", func(t *testing.T) {
+		testData := []int{2, 1, 4, 3, 5}
+
+		bt := BinaryTree{}
+
+		for _, data := range testData {
+			bt.Insert(data)
+		}
+
+		visited := []int{}
+
+		bt.Traverse(PostOrder, func(value int) {
+			visited = append(visited, value)
+		})
+
+		expected := []int{1, 3, 5, 4, 2}
+		for i := range expected {
+			if visited[i] != expected[i] {
+				t.Fatalf("Unexpected: index= %d expected= %v visited= %v", i, expected, visited)
+			}
+		}
+	})
 }
